305486b5a433be25968a9524bbc6ab6307124769
Satriotol <satriotol69@gmail.com>
2019-06-19 21:08:40 +0700

    on web

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,20 @@ Route::get('/home', 'HomeController@index')->name('home');
 // Route::get('/upload/hapus/{id}','UploadController@hapus');
 
 //baru
-// Route::get('/','kompensasiController@home');
-// Route::get('header','kompensasiController@header');
-// Route::get('/ik2a','kompensasiController@ik2a');
-// Route::get('/ik2b','kompensasiController@ik2b');
-// Route::get('tambah','kompensasiController@tambah');
-// Route::post('store','kompensasiController@store');
-// Route::get('edit/{id}','kompensasiController@edit');
-// Route::get('editfoto/{id}','kompensasiController@editfoto');
-// Route::put('update/{id}','kompensasiController@update');
-// Route::put('updatefoto/{id}','kompensasiController@updatefoto');
-// Route::get('hapus/{id}','kompensasiController@hapus');
-// Route::get('about','kompensasiController@about');
-// Route::get('projects','kompensasiController@projects');
-// Route::get('contact','kompensasiController@contact');
+Route::get('/','kompensasiController@home');
+Route::get('header','kompensasiController@header');
+Route::get('/ik2a','kompensasiController@ik2a');
+Route::get('/ik2b','kompensasiController@ik2b');
+Route::get('tambah','kompensasiController@tambah');
+Route::post('store','kompensasiController@store');
+Route::get('edit/{id}','kompensasiController@edit');
+Route::get('editfoto/{id}','kompensasiController@editfoto');
+Route::put('update/{id}','kompensasiController@update');
+Route::put('updatefoto/{id}','kompensasiController@updatefoto');
+Route::get('hapus/{id}','kompensasiController@hapus');
+Route::get('about','kompensasiController@about');
+Route::get('projects','kompensasiController@projects');
+Route::get('contact','kompensasiController@contact');
 
-// Route::get('/siswa', 'SiswaController@index');
-// Route::get('/siswa/export_excel', 'SiswaController@export_excel');
+Route::get('/siswa', 'SiswaController@index');
+Route::get('/siswa/export_excel', 'SiswaController@export_excel');
