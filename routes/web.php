5b0f634df33cfe6dac27dbac970b4fcbdb1e4309
Satriotol <satriotol69@gmail.com>
2019-06-22 20:46:17 +0700

    login session berhasil

--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', 'HomeController@index')->name('home');
+
 // Route::get('/upload','UploadController@upload');
 // Route::post('/upload/proses','UploadController@proses_upload');
 // Route::get('/upload/hapus/{id}','UploadController@hapus');
 
 //baru
-Route::get('header','kompensasiController@header');
-Route::get('/ik2a','kompensasiController@ik2a');
-Route::get('/ik2b','kompensasiController@ik2b');
-Route::get('tambah','kompensasiController@tambah');
-Route::post('store','kompensasiController@store');
-Route::get('edit/{id}','kompensasiController@edit');
-Route::get('editfoto/{id}','kompensasiController@editfoto');
-Route::put('update/{id}','kompensasiController@update');
-Route::put('updatefoto/{id}','kompensasiController@updatefoto');
-Route::get('hapus/{id}','kompensasiController@hapus');
-Route::get('about','kompensasiController@about');
-Route::get('projects','kompensasiController@projects');
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('header','kompensasiController@header');
+    Route::get('/ik2a','kompensasiController@ik2a');
+    Route::get('/ik2b','kompensasiController@ik2b');
+    Route::get('tambah','kompensasiController@tambah');
+    Route::post('store','kompensasiController@store');
+    Route::get('edit/{id}','kompensasiController@edit');
+    Route::get('editfoto/{id}','kompensasiController@editfoto');
+    Route::put('update/{id}','kompensasiController@update');
+    Route::put('updatefoto/{id}','kompensasiController@updatefoto');
+    Route::get('hapus/{id}','kompensasiController@hapus');
+    Route::get('about','kompensasiController@about');
+    Route::get('projects','kompensasiController@projects');
+});
 Route::get('/','kompensasiController@home');
 
 Route::get('/siswa', 'SiswaController@index');
